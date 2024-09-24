6936af5ad93fba39c36e0d826f93ea25860107a8
lukascobit <lukyn.odehnal333@gmail.com>
2024-09-24 12:24:53 +0200

    sorta working

--- ball.gd
+++ ball.gd
@@ -1,11 +1,30 @@
-extends StaticBody2D
+extends CharacterBody2D
 
+# Constants
+const SPEED = 500
 
-# Called when the node enters the scene tree for the first time.
+# Velocity variable
+#var velocity = Vector2()
+
 func _ready() -> void:
-	pass # Replace with function body.
+	rotation = randi()*360
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Set velocity based on the current rotation
+	velocity = Vector2(SPEED, 0).rotated(rotation)
+
+	# Move the character and detect collision
+	var collision = move_and_collide(velocity * delta)
+
+	if collision && collision.get_collider().to_string():
+		print(collision.get_collider())
+		velocity = velocity.bounce(collision.get_normal())
+		rotation = velocity.angle()
+		move_and_collide(velocity)
+
+
+func _on_sides_area_entered(area: Area2D) -> void:
+	position.x = 100
+	position.y = 100
+	
